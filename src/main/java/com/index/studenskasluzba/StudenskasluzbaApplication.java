aa56e0e3aa20b01963fe9557b840d3521df844a7
osmanhadzic <hadzico39@gmail.com>
2020-01-20 11:50:52 +0100

    Initial commit

---
+++ src/main/java/com/index/studenskasluzba/StudenskasluzbaApplication.java
@@ -0,0 +1,13 @@
+package com.index.studenskasluzba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudenskasluzbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudenskasluzbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/index/studenskasluzba/StudenskasluzbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.index.studenskasluzba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudenskasluzbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
